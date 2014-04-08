2cb18e9d961bb72c2f78531fd0a33952369ce7e9
Vincenzo Ferrari <wilk3ert@gmail.com>
2014-04-08 12:05:16 +0200

    added jshint

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+
+gulp.task('lint', function () {
+    return gulp.src('index.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
